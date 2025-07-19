832c15ccd3ac437ac8143fc12a05121f8d40cf7d
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-07-19 13:40:13 -0400

    refactor: mejora la legibilidad del archivo vite.config.js y añade comentarios explicativos

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 
-export default defineConfig({
+// Detecta si estamos en dev-server o build de producción.
+export default defineConfig(({ command }) => ({
+  // En desarrollo: "/", en GitHub Pages: "/productify/"
+  base: command === 'build' ? '/productify/' : '/',
   resolve: {
     alias: {
-      //Facilita el acceso a la carpeta src/ para futuras referencias url en el proyecto.
+      // Facilita el acceso a la carpeta src/
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+}));
